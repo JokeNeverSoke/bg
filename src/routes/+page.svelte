d6083030f7abbef2862fae58f5703c8476042c60
Joseph Zeng <jokeneversoke@gmail.com>
2023-12-12 09:16:36 -0500

    increase letter spacing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <div class="w-screen h-screen bg-primary dark:bg-slate-800">
 	<div
-		class="absolute right-60 bottom-48 font-['Monaspace_Xenon'] font-light text-right text-lg dark:text-primary border-black dark:border-primary border-r-2 px-4"
+		class="absolute right-60 bottom-48 font-['Monaspace_Xenon'] font-extralight tracking-wide text-right text-lg dark:text-primary border-black dark:border-primary border-r-2 px-4"
 	>
 		Therefore do not worry about tomorrow, for<br />
 		tomorrow will worry about itself. Each day has <br />
